f9ff30eda64fcf3d95586c1b55c27f4b8a628498
Geraldi Dzakwan <geraldi.dzakwan@bukalapak.com>
2021-06-28 21:04:01 +0700

    Add config to store filepaths and feature metadata

---
+++ config.py
@@ -0,0 +1,12 @@
+DATA_FILEPATH = "data/products.json"
+INVERTED_INDEX_FILEPATH = "data/inverted_index"
+FEATURES = [
+    {
+        "name": "title",
+        "type": "string"
+    },
+    {
+        "name": "price",
+        "type": "integer"
+    }
+]
