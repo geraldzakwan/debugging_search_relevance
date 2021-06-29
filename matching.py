1879a380f6dac3bedf5bb032fd84138b60da3325
Geraldi Dzakwan <geraldi.dzakwan@bukalapak.com>
2021-06-29 14:25:26 +0700

    Add blueprint for matching.py

---
+++ matching.py
@@ -0,0 +1,9 @@
+# Will return product IDs using AND logic to
+# aggregate results from various fields
+# Now feature is limited to string only
+def match(indexes, fields, search_terms):
+    for field in fields:
+        pass
+
+if __name__ == '__main__':
+    pass
