dfdfeb18cce72606c5c7ce6f9bb4cfbac25b7024
Geraldi Dzakwan <geraldi.dzakwan@gmail.com>
2021-07-01 12:59:57 +0700

    Modify a bit how tf scores are presented (discarding ID key)

--- view.py
+++ view.py
@@ -26,14 +26,20 @@ def fetch_with_score(sorted_products, detail_scores):
 
     result = []
     for idx, ID in enumerate(sorted_product_IDs):
+        tf_scores = {}
+        for feature_name in detail_scores["tf"]:
+            tf_scores[feature_name] = detail_scores["tf"][feature_name][ID]
+
+        idf_scores = detail_scores["idf"]
+
         scores = {
             "final_score": sorted_product_scores[idx],
-            "tf": detail_scores["tf"],
-            "idf": detail_scores["idf"]
+            "tf": tf_scores,
+            "idf": idf_scores
         }
 
         product_data = data[ID]
-        
+
         product_data["ID"] = ID
         product_data["scores"] = scores
 
